51eab0fc01a31f8a82d80fca0bb3f92da9221215
allforfuture <158180827@qq.com>
2020-07-01 17:24:07 +0800

    增加同一路径下运行互斥锁功能

--- JigTimeController/Program.cs
+++ JigTimeController/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Diagnostics;
+using System.Reflection;
+
 namespace JigTimeController
 {
     static class Program
@@ -16,6 +19,11 @@ namespace JigTimeController
         {
             try
             {
+                if (RunningInstance() != null)
+                {
+                    MessageBox.Show("相同路径的该程序已启动", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Environment.Exit(0);
+                }
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new Main());
@@ -25,6 +33,33 @@ namespace JigTimeController
                 MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 Environment.Exit(0);
             }
+        }
+
+        /// <summary>
+        /// 是否运行两次以上（同一路径）
+        /// </summary>
+        /// <returns></returns>
+        public static Process RunningInstance()
+        {
+            Process current = Process.GetCurrentProcess();
+            Process[] processes = Process.GetProcessesByName(current.ProcessName);
+            //Loop through the running processes in with the same name
+            foreach (Process process in processes)
+            {
+                //Ignore the current process
+                if (process.Id != current.Id)
+                {
+                    //Make sure that the process is running from the exe file.
+                    if (Assembly.GetExecutingAssembly().Location.Replace("/", "\\") == current.MainModule.FileName)
+                    {
+                        //if the run path Different then return the other process instance.
+                        if (process.MainModule.FileName == current.MainModule.FileName)
+                            return process;
+                    }
+                }
+            }
+            //No other instance was found, return null.
+            return null;
         }
     }
 }
